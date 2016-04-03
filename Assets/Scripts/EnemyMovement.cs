c3ef768151e2c5383204cbed5e5c0af1ec542139
Arda Arslan <b21383579@cs.hacettepe.edu.tr>
2016-04-03 22:42:24 +0300

    Kule ve saldırısı eklendi
    Sahnedeki düşman ve karakterlere collider eklendi hareket scriptleri
    yazıldı.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	public int speed = 8;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate ()
+	{
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter ( Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+			speed = 0;
+	}
+}
---
+++ Assets/Scripts/TowerAttackScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerAttackScript : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter ( Collider other)
+	{
+		if (other.gameObject.tag == "Enemy") {
+			GameObject ball = Instantiate (ballPrefab, new Vector3 (-129.8f, -247.4f, 191.80f), gameObject.transform.rotation) as GameObject;
+			Rigidbody rb = new Rigidbody ();
+			rb = ball.GetComponent<Rigidbody>();
+			rb.AddForce (1500,5,5);
+		}
+	}
+}
